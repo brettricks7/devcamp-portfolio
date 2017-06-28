876f302049aa0d9a96dbbce8965602fc365319a3
Brett Ricks <brettricks7@gmail.com>
2017-06-28 01:40:49 +0000

    Fixed bugs in show page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,15 +4,15 @@ class PortfoliosController < ApplicationController
   end
 
   def new
-    @portfolio_items = Portfolio.new  
+    @portfolio_item = Portfolio.new
   end
-  
+
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio_items.save
-        format.html { redirect_to portfolios_path, notice: 'Porfolio item is now live.' }
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -20,22 +20,23 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-  @portfolio_items = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
-
   def update
+    @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
-      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-             else
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
         format.html { render :edit }
       end
     end
   end
 
   def show
-    @portfolio_items = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
