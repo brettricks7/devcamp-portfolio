00d4a697d0b9907c3f5070525825d1b965358903
Brett Ricks <brettricks7@gmail.com>
2018-07-31 17:14:04 +0000

    Updated fix for show page image and added edit, and integrated cocoon forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  User.create!(
+    email: "test@test.com",
+    password: "asdfasd",
+    password_confirmation: "asdfasd",
+    name: "Admin User",
+    roles: "site_admin"
+    )
+    
+    puts "1 Admin user created"
+    
+    User.create!(
+    email: "tes2@test.com",
+    password: "asdfasd",
+    password_confirmation: "asdfasd",
+    name: "Regular User",
+    )
+  
+  puts "1 regular user created"
 
   3.times do |topic|
     Topic.create!(
